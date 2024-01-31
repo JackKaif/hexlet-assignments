c50a8babacb68cba3c73bcf05dac5f95d46219d0
JackKaif <zemmer2011@ya.ru>
2024-01-31 18:18:00 +0300

    Add getWordCount() behaviour

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -4,5 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 // BEGIN
-
+class App {
+    public static Map<String, Integer> getWordCount(String sentence) {
+        var wordsFrequency = new HashMap<String, Integer>();
+        if (sentence.isEmpty()) {
+            return wordsFrequency;
+        }
+        var words = sentence.split(" ");
+        for (var word : words) {
+            var count = wordsFrequency.getOrDefault(word, 0);
+            wordsFrequency.put(word, count + 1);
+        }
+        return wordsFrequency;
+    }
+}
 //END
