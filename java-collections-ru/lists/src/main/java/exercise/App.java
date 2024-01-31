1f8cad91eae73d53ff982e551c0d8370f6b42a4f
JackKaif <zemmer2011@ya.ru>
2024-01-31 16:42:25 +0300

    Add scrabble() behaviour

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -5,5 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 // BEGIN
-
+public class App {
+    public static boolean scrabble(String letters, String word){
+        List<String> wordLetters = new ArrayList<String>(Arrays.asList(word.toLowerCase().split("")));
+        List<String> scrabbleLetters = new ArrayList<String>(Arrays.asList(letters.toLowerCase().split("")));
+        for (var letter: wordLetters) {
+            if (!scrabbleLetters.contains(letter)) {
+                return false;
+            }
+            scrabbleLetters.remove(letter);
+        }
+        return true;
+    }
+}
 //END
