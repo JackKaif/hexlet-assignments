cc9da834f2782f80e332402ef1b3d5acff76cee9
JackKaif <zemmer2011@ya.ru>
2024-02-01 11:09:36 +0300

    Add findWhere() behaviour

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -3,8 +3,17 @@ package exercise;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
-import java.util.Map.Entry;
 
 // BEGIN
-
+class App {
+    public static List<Map<String, String>> findWhere(List<Map<String, String>> books, Map<String, String> dictionary) {
+        var overlaps = new ArrayList<Map<String, String>>();
+        for (var book: books) {
+            if (book.entrySet().containsAll(dictionary.entrySet())) {
+                overlaps.add(book);
+            }
+        }
+        return overlaps;
+    }
+}
 //END
