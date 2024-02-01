804936358cf1ce242250b355bbe267208d6ff818
JackKaif <zemmer2011@ya.ru>
2024-02-01 17:06:12 +0300

    Add tests for App.take()

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -1,5 +1,6 @@
 package exercise;
 
+import static exercise.App.take;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.Arrays;
@@ -11,7 +12,19 @@ class AppTest {
     @Test
     void testTake() {
         // BEGIN
-        
+        var list = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+
+        var actual1 = take(list, 2);
+        assertThat(actual1.equals(Arrays.asList(1, 2))).isTrue();
+
+        var actual2 = take(list, 5);
+        assertThat(actual2.equals(Arrays.asList(1, 2, 3, 4))).isTrue();
+
+        var actual3 = take(list, -1);
+        assertThat(actual3.equals(List.of())).isTrue();
+
+        var actual4 = take(List.of(), 2);
+        assertThat(actual4.equals(List.of())).isTrue();
         // END
     }
 }
