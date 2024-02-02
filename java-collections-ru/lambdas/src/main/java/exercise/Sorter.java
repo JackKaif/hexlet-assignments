f0a88ae43b4270dfe0c8884781187d3fa5eefbf1
JackKaif <zemmer2011@ya.ru>
2024-02-02 14:00:12 +0300

    Add takeOldestMans() behaviour

--- java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -1,11 +1,23 @@
 package exercise;
 
-import java.util.Comparator;
 import java.util.Map;
 import java.util.List;
 import java.time.LocalDate;
-import java.util.stream.Collectors;
+import java.time.format.DateTimeFormatter;
 
 // BEGIN
-
+class Sorter {
+    public static List<String> takeOldestMans(List<Map<String, String>> users) {
+        return users.stream()
+                .filter(gender -> gender.get("gender").equals("male"))
+                .sorted((map1, map2) -> {
+                    var dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                    var date1 = LocalDate.parse(map1.get("birthday"), dateFormatter);
+                    var date2 = LocalDate.parse(map2.get("birthday"), dateFormatter);
+                    return date1.compareTo(date2);
+                })
+                .map(name -> name.get("name"))
+                .toList();
+    }
+}
 // END
