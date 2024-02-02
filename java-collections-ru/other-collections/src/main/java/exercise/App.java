ae107742e48e5d95c63e7710eda87154be7d1e35
JackKaif <zemmer2011@ya.ru>
2024-02-02 18:36:49 +0300

    Add genDiff() behaviour

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -2,9 +2,31 @@ package exercise;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeSet;
 
 // BEGIN
-
+class App {
+    public static <T> LinkedHashMap<String, String> genDiff(Map<String, T> firstMap, Map<String, T> secondMap) {
+        var keySet = new TreeSet<String>();
+        firstMap.forEach((k, v) -> keySet.add(k));
+        secondMap.forEach((k, v) -> keySet.add(k));
+        var result = new LinkedHashMap<String, String>();
+        keySet.forEach(key -> {
+            var keyInFirstMap = firstMap.containsKey(key);
+            var keyInSecondMap = secondMap.containsKey(key);
+            if (keyInFirstMap && keyInSecondMap) {
+                if (firstMap.get(key).equals(secondMap.get(key))) {
+                    result.put(key, "unchanged");
+                } else {
+                    result.put(key, "changed");
+                }
+            } else if (keyInFirstMap) {
+                    result.put(key, "deleted");
+            } else if (keyInSecondMap) {
+                result.put(key, "added");
+            }
+        });
+        return result;
+    }
+}
 //END
