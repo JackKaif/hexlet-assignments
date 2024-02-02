3bd161fdfb6cc9cad3fca1fd1c134da214b17433
JackKaif <zemmer2011@ya.ru>
2024-02-02 15:41:42 +0300

    Add brute realization of getForwardedVariables()

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -4,5 +4,17 @@ import java.util.stream.Collectors;
 import java.util.Arrays;
 
 // BEGIN
-
+class App {
+    public static String getForwardedVariables(String file) {
+        var fileStringsList = Arrays.asList(file.split("\\r\n"));
+        return fileStringsList.stream()
+                .filter(string -> string.startsWith("environment"))
+                .map(string -> string.replaceAll("environment=\"", ""))
+                .map(string -> string.replaceAll("\"", ""))
+                .flatMap(string -> Arrays.stream(string.split(",")))
+                .filter(string -> string.startsWith("X_FORWARDED_"))
+                .map(string -> string.replaceAll("X_FORWARDED_", ""))
+                .collect(Collectors.joining(","));
+    }
+}
 //END
