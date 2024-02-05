8a74899b511e29cd49c82a10423a56723b6a01db
JackKaif <zemmer2011@ya.ru>
2024-02-05 16:10:00 +0300

    Implement KeyValueStorage in InMemoryKV, add swapKeyValue in App.java
    Complete subtyping main task.

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -1,9 +1,16 @@
 package exercise;
 
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
+import java.util.HashMap;
 
 // BEGIN
-
+public class App {
+    public static void swapKeyValue(KeyValueStorage data) {
+        var tempMap = new HashMap<String, String>();
+        data.toMap().forEach((k, v) -> {
+            tempMap.put(v, k);
+            data.unset(k);
+        });
+        tempMap.forEach(data::set);
+    }
+}
 // END
--- java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
@@ -4,5 +4,27 @@ import java.util.Map;
 import java.util.HashMap;
 
 // BEGIN
+public class InMemoryKV implements KeyValueStorage {
+    private final HashMap<String, String> data;
 
+    public InMemoryKV(Map<String, String> data) {
+        this.data = new HashMap<>(data);
+    }
+
+    public void set(String key, String value) {
+        this.data.put(key, value);
+    }
+
+    public void unset(String key) {
+        this.data.remove(key);
+    }
+
+    public String get(String key, String defaultValue) {
+        return this.data.getOrDefault(key, defaultValue);
+    }
+
+    public Map<String, String> toMap() {
+        return new HashMap<>(this.data);
+    }
+}
 // END
