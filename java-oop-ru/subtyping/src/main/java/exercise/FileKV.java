216450add6e2dbb5bc5b45ad429707085c578496
JackKaif <zemmer2011@ya.ru>
2024-02-05 17:03:54 +0300

    Implement KeyValueStorage in FileKV, add tests for FileKV, recreate resource file
    Complete subtyping additional task.

--- java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
@@ -1,5 +1,50 @@
 package exercise;
 
 // BEGIN
+import java.util.Map;
 
+class FileKV implements KeyValueStorage {
+
+    private final String filepath;
+
+    FileKV(String filepath, Map<String, String> initial) {
+        this.filepath = filepath;
+        Map<String, String> data = loadData();
+        data.putAll(initial);
+        saveFile(data);
+    }
+
+    private Map<String, String> loadData() {
+        String content = Utils.readFile(this.filepath);
+        Map<String, String> data = Utils.unserialize(content);
+        return data;
+    }
+
+    private void saveFile(Map<String, String> data) {
+        Utils.writeFile(this.filepath, Utils.serialize(data));
+    }
+
+    public void set(String key, String value) {
+        Map<String, String> data = loadData();
+        data.put(key, value);
+        saveFile(data);
+    }
+
+    public void unset(String key) {
+        Map<String, String> data = loadData();
+        data.remove(key);
+        saveFile(data);
+    }
+
+    public String get(String key, String defaultValue) {
+        Map<String, String> data = loadData();
+        return data.getOrDefault(key, defaultValue);
+    }
+
+    public Map<String, String> toMap() {
+        return loadData();
+    }
+
+
+}
 // END
--- java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
+++ java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
@@ -6,9 +6,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.Map;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 // BEGIN
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 // END
 
 
@@ -24,6 +27,17 @@ class FileKVTest {
     }
 
     // BEGIN
-    
+    @Test
+    public void fileKVTest() {
+        KeyValueStorage storage = new FileKV(filepath.toString(), Map.of("key", "value"));
+        assertEquals(Map.of("key", "value"), storage.toMap());
+
+        storage.set("key2", "value2");
+        assertEquals("value", storage.get("key", "default"));
+
+        storage.unset("key");
+        assertEquals("default", storage.get("key", "default"));
+
+    }
     // END
 }
