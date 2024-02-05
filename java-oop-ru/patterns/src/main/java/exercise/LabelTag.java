e14b817ae28220ff9043edaa5e4c0d7c14247b1c
JackKaif <zemmer2011@ya.ru>
2024-02-05 19:08:34 +0300

    Implement TagInterface in InputTag and LabelTag
    Complete patterns task.

--- java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
+++ java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
@@ -1,5 +1,17 @@
 package exercise;
 
 // BEGIN
+public class LabelTag implements TagInterface{
+    private final String label;
+    private final TagInterface taggedText;
 
+    public LabelTag(String label, TagInterface taggedText) {
+        this.label = label;
+        this.taggedText = taggedText;
+    }
+
+    public String render() {
+        return String.format("<label>%s%s</label>", label, taggedText.render());
+    }
+}
 // END
