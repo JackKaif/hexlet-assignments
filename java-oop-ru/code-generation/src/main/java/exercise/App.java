f70ceb5e59ead86acd2e436cb88884a8d2ff4a95
JackKaif <zemmer2011@ya.ru>
2024-02-06 17:13:25 +0300

    Complete code-generation task
    Add lombok annotations in User.java and Car.java.
    Add serialization and deserialization Car object to JSON.
    Add saving and extracting Car object data from file.

--- java-oop-ru/code-generation/src/main/java/exercise/App.java
+++ java-oop-ru/code-generation/src/main/java/exercise/App.java
@@ -5,5 +5,13 @@ import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 
 // BEGIN
+public class App {
+    public static void save(Path filepath, Car car) throws Exception {
+        Files.writeString(filepath, car.serialize(), StandardOpenOption.CREATE);
+    }
 
+    public static Car extract(Path filepath) throws Exception {
+        return Car.unserialize(Files.readString(filepath));
+    }
+}
 // END
--- java-oop-ru/code-generation/src/main/java/exercise/Car.java
+++ java-oop-ru/code-generation/src/main/java/exercise/Car.java
@@ -4,7 +4,7 @@ import lombok.Value;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 // BEGIN
-
+@Value
 // END
 class Car {
     int id;
@@ -14,6 +14,12 @@ class Car {
     User owner;
 
     // BEGIN
-    
+    public String serialize() throws Exception {
+        return new ObjectMapper().writeValueAsString(this);
+    }
+
+    public static Car unserialize(String json) throws Exception {
+        return new ObjectMapper().readValue(json, Car.class);
+    }
     // END
 }
