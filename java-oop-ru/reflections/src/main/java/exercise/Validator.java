187a35f7d88d2c265d59d518898dca6d2eab57d0
JackKaif <zemmer2011@ya.ru>
2024-02-06 15:30:09 +0300

    Complete reflections main task
    Add annotations in Address.java.
    Add validate method in Validator.java

--- java-oop-ru/reflections/src/main/java/exercise/Validator.java
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -1,6 +1,28 @@
 package exercise;
 
 import java.lang.reflect.Field;
-// BEGIN
+import java.util.List;
+import java.util.ArrayList;
 
+// BEGIN
+public class Validator {
+    public static List<String> validate(Object object) {
+        var notValidatedFields = new ArrayList<String>();
+        Field[] fields = object.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            var hasNotNullAnnotation = field.getAnnotation(NotNull.class) != null;
+            try {
+                var value = field.get(object);
+                if (value == null && hasNotNullAnnotation) {
+                    notValidatedFields.add(field.getName());
+                }
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return notValidatedFields;
+    }
+
+}
 // END
