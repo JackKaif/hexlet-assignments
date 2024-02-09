35bff3a64fcf48f4640000faccea2b0cf7d8fa05
JackKaif <zemmer2011@ya.ru>
2024-02-09 12:39:46 +0300

    Complete http-session task
    Add response for /users with page and per params. Return specified page with certain users per page.

--- java-web-ru/http-session/src/main/java/exercise/App.java
+++ java-web-ru/http-session/src/main/java/exercise/App.java
@@ -1,8 +1,13 @@
 package exercise;
 
 import io.javalin.Javalin;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public final class App {
 
@@ -15,7 +20,20 @@ public final class App {
         });
 
         // BEGIN
-        
+        app.get("/users", ctx -> {
+            var pageParam = ctx.queryParam("page");
+            var page = pageParam != null ?
+                    Integer.parseInt(pageParam) :
+                    1;
+            var perParam = ctx.queryParam("per");
+            var per = perParam != null ?
+                    Integer.parseInt(perParam) :
+                    5;
+            var result = IntStream.range((page - 1) * per, page * per)
+                    .mapToObj(USERS::get)
+                    .collect(Collectors.toCollection(ArrayList::new));
+            ctx.json(result);
+        });
         // END
 
         return app;
