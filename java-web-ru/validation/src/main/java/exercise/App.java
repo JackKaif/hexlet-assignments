68ebc4a653f5f047e73240b8cbafeb29041eeef2
JackKaif <zemmer2011@ya.ru>
2024-02-12 19:08:22 +0300

    Complete validation task
    Mark root for jte templates. Add submitting articles.

--- java-web-ru/validation/src/main/java/exercise/App.java
+++ java-web-ru/validation/src/main/java/exercise/App.java
@@ -2,6 +2,9 @@ package exercise;
 
 import io.javalin.Javalin;
 import io.javalin.validation.ValidationException;
+
+import java.util.Comparator;
+import java.util.ConcurrentModificationException;
 import java.util.List;
 import exercise.model.Article;
 import exercise.dto.articles.ArticlesPage;
@@ -29,7 +32,37 @@ public final class App {
         });
 
         // BEGIN
-        
+        app.get("/articles/build", ctx -> {
+            var page = new BuildArticlePage();
+            ctx.render("articles/build.jte", Collections.singletonMap("page", page));
+        });
+
+        app.post("/articles", ctx -> {
+            try {
+                var name = ctx.formParamAsClass("title", String.class)
+                        .check(value -> {
+                            var exists = ArticleRepository.getEntities().stream()
+                                    .map(Article::getTitle)
+                                    .filter(value::equals)
+                                    .findFirst()
+                                    .orElse(null);
+                            return exists == null;
+                        }, "Статья с таким названием уже существует")
+                        .check(value -> value.length() > 2, "Название не должно быть короче двух символов")
+                        .get();
+                var content = ctx.formParamAsClass("content", String.class)
+                        .check(value -> value.length() > 10, "Статья должна быть не короче 10 символов")
+                        .get();
+                ArticleRepository.save(new Article(name, content));
+                ctx.redirect("/articles");
+            } catch (ValidationException e) {
+                var page = new BuildArticlePage(ctx.formParam("title"),
+                        ctx.formParam("content"),
+                        e.getErrors());
+                ctx.status(422);
+                ctx.render("articles/build.jte", Collections.singletonMap("page", page));
+            }
+        });
         // END
 
         return app;
