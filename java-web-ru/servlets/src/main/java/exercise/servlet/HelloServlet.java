549b0e1783a4ced14a90fab687b4b5fb5e5557b1
JackKaif <zemmer2011@ya.ru>
2024-02-14 19:49:15 +0300

    Add servlet for GET query.
    URL "/hello"

--- java-web-ru/servlets/src/main/java/exercise/servlet/HelloServlet.java
+++ java-web-ru/servlets/src/main/java/exercise/servlet/HelloServlet.java
@@ -1,6 +1,7 @@
-package io.hexlet.servlet;
+package exercise.servlet;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,6 +12,15 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet(name = "HelloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     // BEGIN
-    
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+        var sb = new StringBuilder("Hello, ");
+        var name = request.getParameter("name");
+        sb.append(Objects.requireNonNullElse(name, " Guest"));
+        sb.append("!");
+        request.setAttribute("message", sb.toString());
+        request.getRequestDispatcher("WEB-INF/hello.jsp").forward(request, response);
+    }
     // END
 }
