2eb4bf3dcc2fdc827c72349548a5af4007475edd
JackKaif <zemmer2011@ya.ru>
2024-02-16 14:42:59 +0300

    Complete rest api task

--- java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -2,17 +2,30 @@ package exercise.controller.users;
 
 import java.util.List;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import exercise.model.Post;
 import exercise.Data;
 
 // BEGIN
+@RestController
+@RequestMapping("/api")
+public class PostsController {
+    private static final List<Post> posts = Data.getPosts();
+    @GetMapping("/users/{id}/posts")
+    public static List<Post> index(@PathVariable String id) {
+        return posts.stream()
+                .filter(post -> id.equals(String.valueOf(post.getUserId())))
+                .toList();
+    }
 
+    @PostMapping("/users/{id}/posts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public static Post create(@PathVariable String id,
+                              @RequestBody Post post) {
+        post.setUserId(Integer.parseInt(id));
+        posts.add(post);
+        return post;
+    }
+}
 // END
