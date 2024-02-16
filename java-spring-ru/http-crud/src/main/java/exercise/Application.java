09f20f1397ae1f94a7270eb2d80dea19f9b41fb0
JackKaif <zemmer2011@ya.ru>
2024-02-16 12:35:54 +0300

    Complete http-crud task

--- java-spring-ru/http-crud/src/main/java/exercise/Application.java
+++ java-spring-ru/http-crud/src/main/java/exercise/Application.java
@@ -27,6 +27,42 @@ public class Application {
     }
 
     // BEGIN
-    
+    @GetMapping("/posts")
+    public List<Post> index(@RequestParam(defaultValue = "1") Integer page,
+                            @RequestParam(defaultValue = "10") Integer limit) {
+        return posts.stream()
+                .skip((long) (page - 1) * limit)
+                .limit(limit)
+                .toList();
+    }
+
+    @GetMapping("/posts/{id}")
+    public Optional<Post> show(@PathVariable String id) {
+        return posts.stream()
+                .filter(post -> id.equals(post.getId()))
+                .findFirst();
+    }
+
+    @PostMapping("/posts")
+    public Post create(@RequestBody Post post) {
+        posts.add(post);
+        return post;
+    }
+
+    @PutMapping("/posts/{id}")
+    public Post edit(@RequestBody Post editedPost, @PathVariable String id) throws Exception {
+        var editingPost = posts.stream()
+                .filter(post -> id.equals(post.getId()))
+                .findFirst()
+                .orElseThrow(Exception::new);
+        editingPost.setTitle(editedPost.getTitle());
+        editingPost.setBody(editedPost.getBody());
+        return editingPost;
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public void delete(@PathVariable String id) {
+        posts.removeIf(post -> id.equals(post.getId()));
+    }
     // END
 }
