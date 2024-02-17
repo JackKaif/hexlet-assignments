670093b31b97ddea65b4d78dfd5b933e4f5af6a5
JackKaif <zemmer2011@ya.ru>
2024-02-17 18:29:20 +0300

    Complete dto mapper task

--- java-spring-ru/dto-mapper/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/dto-mapper/src/main/java/exercise/controller/ProductsController.java
@@ -27,6 +27,47 @@ public class ProductsController {
     private ProductRepository productRepository;
 
     // BEGIN
-    
+    @Autowired
+    private ProductMapper productMapper;
+
+    @GetMapping("")
+    public List<ProductDTO> index() {
+        return productRepository.findAll().stream()
+                .map(product -> productMapper.map(product))
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public ProductDTO show(@PathVariable Long id) {
+        var product = productRepository.findById(id)
+                .orElseThrow(() -> {
+                    throw new ResourceNotFoundException(
+                            String.format("Product with id %d not found", id)
+                    );
+                });
+        return productMapper.map(product);
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductDTO create(@RequestBody ProductCreateDTO newProduct) {
+        var product = productMapper.map(newProduct);
+        productRepository.save(product);
+        return productMapper.map(product);
+    }
+
+    @PutMapping("/{id}")
+    public ProductDTO update(@PathVariable Long id,
+                             @RequestBody ProductUpdateDTO editedProduct) {
+        var product = productRepository.findById(id)
+                .orElseThrow(() -> {
+                    throw new ResourceNotFoundException(
+                            String.format("Product with id %d not found", id)
+                    );
+                });
+        productMapper.update(editedProduct, product);
+        productRepository.save(product);
+        return productMapper.map(product);
+    }
     // END
 }
--- java-spring-ru/dto-mapper/src/main/java/exercise/mapper/ProductMapper.java
+++ java-spring-ru/dto-mapper/src/main/java/exercise/mapper/ProductMapper.java
@@ -2,4 +2,32 @@ package exercise.mapper;
 
 // BEGIN
 
+import exercise.dto.ProductCreateDTO;
+import exercise.dto.ProductDTO;
+import exercise.dto.ProductUpdateDTO;
+import exercise.model.Product;
+import org.mapstruct.*;
+
+@Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class ProductMapper {
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "cost", source = "price")
+    @Mapping(target = "barcode", source = "vendorCode")
+    public abstract Product map(ProductDTO dto);
+
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "cost", source = "price")
+    @Mapping(target = "barcode", source = "vendorCode")
+    public abstract Product map(ProductCreateDTO newProduct);
+
+    @InheritInverseConfiguration
+    public abstract ProductDTO map(Product product);
+
+    @Mapping(target = "cost", source = "price")
+    public abstract void update(ProductUpdateDTO editedProduct,
+                                @MappingTarget Product product);
+}
 // END
