b973d878bb7e2e9c67c67346abcae8ae94d26a35
JackKaif <zemmer2011@ya.ru>
2024-02-21 13:21:49 +0300

    Complete sync primitives task

--- java-advanced-ru/sync-primitives/src/main/java/exercise/ListThread.java
+++ java-advanced-ru/sync-primitives/src/main/java/exercise/ListThread.java
@@ -1,5 +1,22 @@
 package exercise;
 
 // BEGIN
+public class ListThread extends Thread {
+    SafetyList list;
+    public ListThread(SafetyList list) {
+        this.list = list;
+    }
 
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++){
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            list.add((int) (Math.random() * 1000));
+        }
+    }
+}
 // END
--- java-advanced-ru/sync-primitives/src/main/java/exercise/SafetyList.java
+++ java-advanced-ru/sync-primitives/src/main/java/exercise/SafetyList.java
@@ -2,6 +2,25 @@ package exercise;
 
 class SafetyList {
     // BEGIN
-    
+    int[] array;
+    int size;
+
+    SafetyList() {
+        array = new int[2100];
+        size = 0;
+    }
+
+    synchronized void add(int number) {
+        array[size] = number;
+        size++;
+    }
+
+    public int get(int index) {
+        return index > size ? 0 : array[index];
+    }
+
+    public int getSize() {
+        return this.size;
+    }
     // END
 }
